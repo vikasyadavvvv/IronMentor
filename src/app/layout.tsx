a3da84e7da6e0760d665a7623096195776a88d35
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-05-15 15:26:11 +0530

    TITLE UPDATE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "CodeFlex AI - Get Jacked",
+  title: "IronMentor AI ",
   description: "A modern fitness AI platform to get jacked for free.",
 };
 
